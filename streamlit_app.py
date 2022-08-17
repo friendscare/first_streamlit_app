b506effb3bfb97365b36422f4b2e8875c15ec0d1
friendscare <98495563+friendscare@users.noreply.github.com>
2022-08-17 09:23:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,10 @@ from urllib.error import URLError
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("insert into fruit_load_list values('from streamlit')")
+my_cur.execute("insert into fruit_load_list values('jackfruit')")
+my_cur.execute("insert into fruit_load_list values('papaya')")
+my_cur.execute("insert into fruit_load_list values('kiwi')")
+my_cur.execute("insert into fruit_load_list values('guava')")
+
+
+
